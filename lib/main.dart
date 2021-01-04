acdfdfc42e3939fae44a4bf40a4c1d605d74d06b
Gopal Kaul <gopalkaul5@gmail.com>
2021-01-04 14:46:34 +0530

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -14,23 +13,50 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int temperature = 0;
-  int woeid = 0;
-  int weather;
-  String location = 'Hyderabad';
+  int weather = 0;
+  String desc = '';
+  String location = 'Delhi';
   String apisite = 'https://api.openweathermap.org/data/2.5/weather?q=';
   String apikey = '&appid=5a8737d953ee376f648468efa28a0b4d';
-  void fetchweather(String loc) async {
-    var resp = await http.get(apisite + loc + apikey);
-    var t = jsonDecode(resp.body);
-    var data = t['main']['temp'];
-    var temp = data - 273.16;
-    var weatherdata = t['weather'][0]['id'];
-    temp = temp.round();
-    setState(() {
-      location = loc;
-      temperature = temp;
-      weather = weatherdata;
-    });
+  String iconsite = 'http://openweathermap.org/img/wn/';
+  String iconloc = '';
+  String colour = 'white';
+  String errormsg = '';
+
+  TextStyle tempstyle(int size) {
+    return TextStyle(
+        fontSize: size.toDouble(),
+        color: (colour == 'white') ? Colors.white : Colors.black);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchweather(location);
+  }
+
+  Future fetchweather(String loc) async {
+    try {
+      var resp = await http.get(apisite + loc + apikey);
+      var t = jsonDecode(resp.body);
+      var data = t['main']['temp'];
+      var temp = data - 273.16;
+      var weatherdata = t['weather'][0]['id'];
+      var icon = t['weather'][0]['icon'];
+      var weatherdesc = t['weather'][0]['description'];
+      temp = temp.round();
+      setState(() {
+        location = loc;
+        temperature = temp;
+        weather = weatherdata;
+        iconloc = iconsite + icon.toString() + '@4x.png';
+        desc = weatherdesc;
+      });
+    } catch (e) {
+      setState(() {
+        errormsg = "You have entered an incorrect location! Please retry!";
+      });
+    }
   }
 
   @override
@@ -40,58 +66,114 @@ class _MyAppState extends State<MyApp> {
         home: Container(
           decoration: BoxDecoration(
               image: DecorationImage(
-                  image: AssetImage('images/clear.jpg'), fit: BoxFit.cover)),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Column(
-                  children: [
-                    Center(
-                        child: Text(
-                      temperature.toString() + " °C",
-                      style: TextStyle(fontSize: 60.0, color: Colors.white),
-                    )),
-                    Center(
-                      child: Text(
-                        location,
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  image: AssetImage(weatherlink(weather)), fit: BoxFit.cover)),
+          child: temperature == 0
+              ? Center(child: CircularProgressIndicator())
+              : Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        children: [
+                          if (iconloc != '')
+                            Center(
+                              child: Image(
+                                image: NetworkImage(iconloc),
+                              ),
+                            ),
+                          Center(
+                              child: Text(
+                            temperature.toString() + " °C",
+                            style: tempstyle(50),
+                          )),
+                          Center(
+                            child: Text(
+                              location,
+                              style: tempstyle(40),
+                            ),
+                          ),
+                          if (desc != '')
+                            Center(
+                              child: Text(
+                                capitalize(desc.toString()),
+                                style: tempstyle(40),
+                              ),
+                            )
+                        ],
                       ),
-                    ),
-                  ],
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            width: 400.0,
+                            child: Center(
+                              child: TextField(
+                                onSubmitted: (String input) {
+                                  submitted(input);
+                                },
+                                style: tempstyle(30),
+                                decoration: InputDecoration(
+                                    hintText: "City, Country",
+                                    hintStyle: tempstyle(30),
+                                    prefixIcon: Icon(
+                                      Icons.search,
+                                      color: (colour == 'white')
+                                          ? Colors.white
+                                          : Colors.black,
+                                    )),
+                              ),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              errormsg,
+                              style:
+                                  TextStyle(fontSize: 20.0, color: Colors.red),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-                Column(
-                  children: <Widget>[
-                    Container(
-                      width: 400.0,
-                      child: Center(
-                        child: TextField(
-                          onSubmitted: (String input) {
-                            submitted(input);
-                          },
-                          style: TextStyle(fontSize: 30.0, color: Colors.white),
-                          decoration: InputDecoration(
-                              hintText: "City, Country",
-                              hintStyle: TextStyle(
-                                  fontSize: 30.0, color: Colors.white),
-                              prefixIcon: Icon(
-                                Icons.search,
-                                color: Colors.white,
-                              )),
-                        ),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
         ));
   }
 
-  void submitted(String input) {
-    fetchweather(input);
+  void submitted(String input) async {
+    await fetchweather(input);
   }
+
+  String weatherlink(int weatherid) {
+    if (weatherid == 0) {
+      if (colour != 'white') setcolour('white');
+      return "./images/clear.jpg";
+    } else if (weatherid >= 200 && weatherid < 300) {
+      if (colour != 'white') setcolour('white');
+      return "./images/thunder.jpg";
+    } else if (weatherid >= 300 && weatherid < 400) {
+      if (colour != 'white') setcolour('white');
+      return "./images/drizzle.jpg";
+    } else if (weatherid >= 500 && weatherid < 600) {
+      if (colour != 'white') setcolour('white');
+      return "./images/rain.jpg";
+    } else if (weatherid >= 600 && weatherid < 700) {
+      if (colour != 'black') setcolour('black');
+      return "./images/snow.jpg";
+    } else if (weatherid >= 800 && weatherid < 900) {
+      if (colour != 'white') setcolour('white');
+      return "./images/cloudy.jpg";
+    } else {
+      if (colour != 'white') setcolour('white');
+      return './images/clear.jpg';
+    }
+  }
+
+  void setcolour(String color) {
+    setState(() {
+      colour = color;
+    });
+  }
+
+  String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
 }
